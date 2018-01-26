7dc7f2b60349c548da85a4543c505eb1e00994c4
yizenghui <zenghuitrue@gmail.com>
2018-01-27 01:04:09 +0800

    openid重复

--- db.go
+++ db.go
@@ -22,10 +22,10 @@ type Fans struct {
 // Feedback 粉丝
 type Feedback struct {
 	ID        uint   `gorm:"primary_key"`
-	OpenID    string `gorm:"type:varchar(255);unique_index"` // 微信文章地址
-	FormID    string `gorm:"type:varchar(255);"`             //订阅formID，一次订阅只能推送一次通知
-	Problem   string `gorm:"type:text;"`                     // 问题
-	Answer    string `gorm:"type:text;"`                     // 答复
+	OpenID    string `gorm:"type:varchar(255);index"` // 微信文章地址
+	FormID    string `gorm:"type:varchar(255);"`      //订阅formID，一次订阅只能推送一次通知
+	Problem   string `gorm:"type:text;"`              // 问题
+	Answer    string `gorm:"type:text;"`              // 答复
 	Show      bool   //是否显示
 	CreatedAt time.Time
 	UpdatedAt time.Time
