ddf9a32b8b8e4074d3abf344685dd453ac430a7e
Administrator <245561237@qq.com>
2018-02-02 12:28:18 +0800

    ss

--- list_test.go
+++ list_test.go
@@ -3,6 +3,8 @@ package minappapi
 import "testing"
 
 func Test_GetList(t *testing.T) {
-	l2, _ := GetList("http://book.zongheng.com/showchapter/523438.html")
+	// l2, _ := GetList("http://book.zongheng.com/showchapter/523438.html")
+	// l2, _ := GetList("http://www.3dllc.cc/html/86/86244/")
+	l2, _ := GetList("http://longfu8.com/")
 	t.Fatal(l2)
 }
