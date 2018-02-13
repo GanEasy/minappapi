af9656288fb398db7492a94000e4c349316af3f9
Administrator <245561237@qq.com>
2018-02-13 16:41:05 +0800

    ss

--- content_test.go
+++ content_test.go
@@ -3,6 +3,6 @@ package minappapi
 import "testing"
 
 func Test_GetContent(t *testing.T) {
-	a2, _ := GetContent("http://book.zongheng.com/showchapter/523438.html")
+	a2, _ := GetContent("http://book.zongheng.com/showchapter/730066.html")
 	t.Fatal(a2)
 }
--- list_test.go
+++ list_test.go
@@ -5,6 +5,15 @@ import "testing"
 func Test_GetList(t *testing.T) {
 	// l2, _ := GetList("http://book.zongheng.com/showchapter/523438.html")
 	// l2, _ := GetList("http://www.3dllc.cc/html/86/86244/")
-	l2, _ := GetList("http://longfu8.com/")
+	// l2, _ := GetList("http://longfu8.com/")
+	l2, _ := GetList("http://book.zongheng.com/showchapter/730066.html")
+	t.Fatal(l2)
+}
+
+func Test_GetBookMenu(t *testing.T) {
+	// l2, _ := GetList("http://book.zongheng.com/showchapter/523438.html")
+	// l2, _ := GetList("http://www.3dllc.cc/html/86/86244/")
+	// l2, _ := GetList("http://longfu8.com/")
+	l2, _ := GetBookMenu("http://book.zongheng.com/showchapter/730066.html")
 	t.Fatal(l2)
 }
