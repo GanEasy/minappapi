5bde7d8b2fe23322bf4920b875cdd4627925032d
Administrator <245561237@qq.com>
2018-04-02 16:39:53 +0800

    实时关注超10个人才出榜(审核不通过)

--- content_test.go
+++ content_test.go
@@ -3,6 +3,7 @@ package minappapi
 import "testing"
 
 func Test_GetContent(t *testing.T) {
-	a2, _ := GetContent("http://book.zongheng.com/showchapter/730066.html")
+	// a2, _ := GetContent("http://book.zongheng.com/showchapter/730066.html")
+	a2, _ := GetContent("http://www.biquge5.com/0_9/5018.html")
 	t.Fatal(a2)
 }
--- list_test.go
+++ list_test.go
@@ -14,6 +14,7 @@ func Test_GetBookMenu(t *testing.T) {
 	// l2, _ := GetList("http://book.zongheng.com/showchapter/523438.html")
 	// l2, _ := GetList("http://www.3dllc.cc/html/86/86244/")
 	// l2, _ := GetList("http://longfu8.com/")
-	l2, _ := GetBookMenu("http://book.zongheng.com/showchapter/730066.html")
+	// l2, _ := GetBookMenu("http://book.zongheng.com/showchapter/730066.html")
+	l2, _ := GetBookMenu("http://www.biquge5.com/0_9/")
 	t.Fatal(l2)
 }
